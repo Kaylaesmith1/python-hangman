779ae2e1a2b4361d83b582455beb6b06578ae8ed
Kayla S <kaylaesmith1@gmail.com>
2022-09-13 17:29:20 +0000

    Solicit user input letter, print prompt.

--- run.py
+++ run.py
@@ -9,7 +9,20 @@ def get_word(words):
     """
     word = random.choice(words)
 
-    while ' ' in word or '-' in word:
+    while ' ' in word or '-' in word: #keeps looking if word has hypen or space
         word = random.choice(words)
 
-    return word
+    return word.upper()
+
+def hangman_game():
+    word = get_word(words)
+    letters_needed = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    letters_guessed = set() #letters user guesses
+
+    #get user input
+    user_guess = input('Guess a letter: ').upper()
+
+user_input = input('Please type a letter: ')
+print(user_input)
+
