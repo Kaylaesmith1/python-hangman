9902cdb97d4d864eed851a993a81a751fe73bd27
Kayla S <kaylaesmith1@gmail.com>
2022-09-13 17:00:11 +0000

    Initial commit.

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import words
+
+
+def get_word(words):
+    """ 
+    Choose random word from 'words.py' file
+    skip 'invalid' words (hyphenated or spaced)
+    """
+    word = random.choice(words)
+
+    while ' ' in word or '-' in word:
+        word = random.choice(words)
+
+    return word
