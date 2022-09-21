39a1d4c9f70d681ca2356f8400958d99adccc6a1
Kayla S <kaylaesmith1@gmail.com>
2022-09-21 13:19:47 +0000

    Pep8 code cleaning. Start lives left visual and function.

--- run.py
+++ run.py
@@ -1,40 +1,50 @@
 import random
 from words import words
-
-#!!ADD COLORS, ADD LEVELS (BASED ON NUMBER OF LIVES LEFT OR BASED ON WORD LENGTH?)
+from hangedman import lives_left
+import string
 
 
 def get_word(words):
-    """ 
+    """
     Choose random word from 'words.py' file
     skip 'invalid' words (hyphenated or spaced)
     """
     word = random.choice(words)
 
-    while ' ' in word or '-' in word: #keeps looking if word has hyphen or space
+    while ' ' in word or '-' in word:  # still look if word has hyphen / space
         word = random.choice(words)
 
     return word.upper()
 
+
 def hangman_game():
     word = get_word(words)
-    letters_needed = set(word) # letters in the word
+    letters_needed = set(word)  # letters in the word
     alphabet = set(string.ascii_uppercase)
-    letters_guessed = set() #letters user guesses
+    letters_guessed = set()  # letters user guesses
 
-    #get user input
+    lives = 7
+
+    # get user input
+    while len(letters_needed) > 0 and lives > 0:
+        print('Lives left:', lives, )
+        print("You've used these letters: ", ' '.join(letters_guessed))
+
+        word_guess = [letter if letter in letters_guessed else '-' for letter in word]
+        print(lives_left[lives])
+        print('Current word: ', ' '.join(word_guess))
+
     user_guess = input('Guess a letter: ').upper()
     if user_guess in alphabet - letters_guessed:
         letters_guessed.add(user_guess)
         if user_guess in word - letters_needed:
             letters_needed.remove(user_guess)
-    
+
     elif user_guess in letters_guessed:
-        print("You've guessed this letter already. Please try again.")
-    
-    else: 
+        print("\nYou've guessed this letter already. Please try again.")
+
+    else:
         print('Error. Please type in a valid letter.')
 
 user_input = input('Please type a letter: ')
 print(user_input)
-
